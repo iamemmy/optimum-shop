7ab6240c96b34244985d6735b189c774f5ebbd31
Samson Eze <38159339+iamemmy@users.noreply.github.com>
2023-05-01 10:36:07 +0100

    All files uploaded
    All files uploaded

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Bluetooth",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Smart Tv",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "S9 Plus Mobile",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Sneeker 46'",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+  {
+    id: "thhfjcbcv",
+    name: "Bicycle Red",
+    price: 261,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "thyfttyeh",
+    name: "HP Pavilion",
+    price: 640,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-6.jpg",
+  },
+  {
+    id: "mkklhcbcv",
+    name: "Headphone JBL",
+    price: 102,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-7.jpg",
+  },
+  {
+    id: "thetrrbcv",
+    name: "Wristwatch Men",
+    price: 700,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-8.jpg",
+  },
+];
